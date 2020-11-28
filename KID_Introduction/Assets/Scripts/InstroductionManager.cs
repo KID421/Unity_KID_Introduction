a0c133df14570e6e84f5a3024081764feb0643d1
KID <50666207+KID421@users.noreply.github.com>
2020-11-28 23:50:53 +0800

    完成發佈網頁版

--- KID_Introduction/Assets/Scripts/InstroductionManager.cs
+++ KID_Introduction/Assets/Scripts/InstroductionManager.cs
@@ -20,10 +20,18 @@ public class InstroductionManager : MonoBehaviour
     public GameObject objBeer;
     [Header("攝影機")]
     public Transform traCamera;
+    [Header("光點")]
+    public Transform traParticle;
     [Header("磚塊群")]
     public GameObject[] bricks;
     [Header("金幣群")]
     public GameObject[] coins;
+    [Header("旗子")]
+    public GameObject flag;
+    [Header("彩帶")]
+    public ParticleSystem parColorPaper;
+    [Header("信箱")]
+    public GameObject email;
 
     private Vector2[] posKIDGo =
     {
@@ -32,7 +40,9 @@ public class InstroductionManager : MonoBehaviour
         new Vector2(16, -6.2f),
         new Vector2(20, -2.7f),
         new Vector2(26, -2.7f),
-        new Vector2(50, 0),
+        new Vector2(179, -2.7f),
+        new Vector2(179, -6.2f),
+        new Vector2(200, -6.2f),
     };
     private Vector2[] posKIDLogo =
     {
@@ -41,9 +51,17 @@ public class InstroductionManager : MonoBehaviour
 
     private void Awake()
     {
-        Time.timeScale = 1;
         btnStart.onClick.AddListener(() => StartCoroutine(PageOneFlow()));
         btnNext.onClick.AddListener(() => StartCoroutine(PageTwoFlow()));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Time.timeScale = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) Time.timeScale = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) Time.timeScale = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha4)) Time.timeScale = 4;
+        if (Input.GetKeyDown(KeyCode.Alpha5)) Time.timeScale = 5;
     }
 
     private IEnumerator PageOneFlow()
@@ -81,8 +99,11 @@ public class InstroductionManager : MonoBehaviour
         while (Vector2.Distance(aniLogo.transform.position , posKIDLogo[0]) > 0.3f)
         {
             aniLogo.transform.Translate(-7 * Time.deltaTime, 0, 0);
+            traParticle.Translate(-7 * Time.deltaTime, 0, 0);
             yield return null;
         }
+
+        aniLogo.transform.position = posKIDLogo[0];
 
         // 停止跑步並揮手
         aniKID.SetBool("跑步開關", false);
@@ -98,12 +119,14 @@ public class InstroductionManager : MonoBehaviour
         yield return new WaitForSeconds(2f);
         aniKID.SetBool("跑步開關", true);
 
-        while (Vector2.Distance(aniKID.transform.position, posKIDGo[1]) > 0.1f)
+        while (Vector2.Distance(aniKID.transform.position, posKIDGo[1]) > 0.3f)
         {
             aniKID.transform.Translate(5 * Time.deltaTime, 0, 0);
             traCamera.Translate(5 * Time.deltaTime, 0, 0);
             yield return null;
         }
+
+        aniKID.transform.position = posKIDGo[1];
 
         // 跳躍並顯示金牌
         aniKID.SetBool("跑步開關", false);
@@ -125,12 +148,14 @@ public class InstroductionManager : MonoBehaviour
         btnNext.gameObject.SetActive(false);
         aniKID.SetBool("跑步開關", true);
 
-        while (Vector2.Distance(aniKID.transform.position, posKIDGo[2]) > 0.1f)
+        while (Vector2.Distance(aniKID.transform.position, posKIDGo[2]) > 0.3f)
         {
             aniKID.transform.Translate(5 * Time.deltaTime, 0, 0);
             traCamera.Translate(5 * Time.deltaTime, 0, 0);
             yield return null;
         }
+
+        aniKID.transform.position = posKIDGo[2];
 
         aniKID.SetBool("跑步開關", false);
         yield return new WaitForSeconds(1);
@@ -146,12 +171,14 @@ public class InstroductionManager : MonoBehaviour
         yield return new WaitForSeconds(0.3f);
         aniKID.SetTrigger("跳躍");
 
-        while (Vector2.Distance(aniKID.transform.position, posKIDGo[3]) > 0.1f)
+        while (Vector2.Distance(aniKID.transform.position, posKIDGo[3]) > 0.3f)
         {
             aniKID.transform.position = Vector2.Lerp(aniKID.transform.position, posKIDGo[3], Time.deltaTime * 3);
             traCamera.Translate(5 * Time.deltaTime, 0, 0);
             yield return null;
         }
+
+        aniKID.transform.position = posKIDGo[3];
 
         yield return new WaitForSeconds(1);
         aniKID.SetBool("跑步開關", true);
@@ -161,6 +188,8 @@ public class InstroductionManager : MonoBehaviour
             traCamera.Translate(5 * Time.deltaTime, 0, 0);
             yield return null;
         }
+
+        aniKID.transform.position = posKIDGo[4];
 
         aniKID.SetBool("跑步開關", false);
         yield return new WaitForSeconds(1);
@@ -184,16 +213,74 @@ public class InstroductionManager : MonoBehaviour
     {
         // 隱藏按鈕並開始跑向右邊
         btnNext.gameObject.SetActive(false);
-        //aniKID.SetBool("跑步開關", true);
+        aniKID.SetBool("飛行", true);
 
-        while (Vector2.Distance(aniKID.transform.position, posKIDGo[5]) > 0.1f)
+        StartCoroutine(EatCoin());
+
+        while (Vector2.Distance(aniKID.transform.position, posKIDGo[5]) > 0.3f)
         {
-            aniKID.transform.position = Vector2.Lerp(aniKID.transform.position, posKIDGo[5], Time.deltaTime * 2);
-            Vector3 posKID = aniKID.transform.position;
-            posKID.y = traCamera.position.y;
-            posKID.z = -10;
-            traCamera.position = Vector3.Lerp(traCamera.position, posKID, Time.deltaTime * 2);
+            aniKID.transform.Translate(5 * Time.deltaTime, 0, 0);
+            traCamera.Translate(5 * Time.deltaTime, 0, 0);
             yield return null;
+        }
+
+        aniKID.transform.position = posKIDGo[5];
+
+        aniKID.SetBool("飛行", false);
+        flag.SetActive(true);
+
+        while (Vector2.Distance(aniKID.transform.position, posKIDGo[6]) > 0.3f)
+        {
+            aniKID.transform.Translate(0, -5 * Time.deltaTime, 0);
+            yield return null;
+        }
+
+        aniKID.transform.position = posKIDGo[6];
+        parColorPaper.Play();
+
+        // 下一頁按鈕事件改為第三頁並顯示按鈕
+        btnNext.onClick.RemoveAllListeners();
+        yield return new WaitForSeconds(2);
+        btnNext.onClick.AddListener(() => StartCoroutine(PageFourFive()));
+        btnNext.gameObject.SetActive(true);
+    }
+
+    private IEnumerator PageFourFive()
+    {
+        // 隱藏按鈕並開始跑向右邊
+        btnNext.gameObject.SetActive(false);
+        aniKID.SetBool("跑步開關", true);
+
+        aniLogo.transform.position = new Vector3(200, 1);
+        aniLogo.gameObject.SetActive(false);
+
+        while (Vector2.Distance(aniKID.transform.position, posKIDGo[7]) > 0.3f)
+        {
+            aniKID.transform.Translate(5 * Time.deltaTime, 0, 0);
+            traCamera.Translate(5 * Time.deltaTime, 0, 0);
+            yield return null;
+        }
+
+        aniKID.transform.position = posKIDGo[7];
+        aniKID.SetBool("跑步開關", false);
+
+        aniLogo.gameObject.SetActive(true);
+        aniLogo.SetTrigger("開始");
+
+        yield return new WaitForSeconds(0.3f);
+        aniKID.SetTrigger("招手");
+        yield return new WaitForSeconds(0.8f);
+        email.SetActive(true);
+    }
+
+    private IEnumerator EatCoin()
+    {
+        yield return new WaitForSeconds(0.6f);
+
+        for (int i = 0; i < coins.Length; i++)
+        {
+            coins[i].SetActive(false);
+            yield return new WaitForSeconds(0.35f);
         }
     }
 }
